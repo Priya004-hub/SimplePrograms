61c21ffbdcdb8892fce52b7456cc60c67b5f1db8
Priya004_hub <rspdd2004@gmail.com>
2024-11-23 14:12:09 +0530

    Question.java file added

---
+++ Question.java
@@ -0,0 +1,21 @@
+//Increament Numbers
+class Number{
+    public void increment(){
+        int num=1;
+        for(int i=0;i<=3;i++){
+            System.out.println("Number :"+ num);
+            num++;
+            
+        }
+        
+    }
+}
+public class Question{
+    public static void main(String[] args){
+        Number number=new Number();
+        number.increment();
+
+            
+        
+    }
+}
